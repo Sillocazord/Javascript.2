88740622dda1f2ee34f6cb379344f6e15c3285a7
Sillas Endrigo <sillas.silva@aluno.senai.br>
2025-04-17 11:58:43 -0300

    feat: realizando exercícios com logíca de programação em js

--- Javascript/Lógica/Exercício03.js
+++ Javascript/Lógica/Exercício03.js
@@ -1,4 +1,7 @@
 var prompt = require('prompt-sync')();
-let frase = prompt("Digite uma frase: ")
-let letra = prompt("Digite uma letra: ")
-let FraseIt
+let frase = prompt("Digite uma frase: ");
+let letra = prompt("Digite uma letra: ").toLowerCase();
+var palavras = frase.split(" ")
+var letraselecionada = palavras.filter(palavra => palavra.toLowerCase().startsWith(letra));
+
+console.log(`Numero de palavras que começam com a letra "${letra}" : ${letraselecionada.length}`)
---
+++ Javascript/Lógica/Exercício04.js
@@ -0,0 +1,6 @@
+function gerarsenha(gerarSenha){
+    let textin = "";
+    for(let i = 0; i < gerarSenha; i++){
+    let codigo = Math
+    }
+}
